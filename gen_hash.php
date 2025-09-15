15319061ef81c0824213e9b90f65ee375cb627d8
unknown <martinp@dh16.digihub.local>
2025-09-15 11:19:34 +0100

    acitentaly added files to wrong repo

--- gen_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($argv[1])) {
-    echo "Password: " . $argv[1] . "\n";
-    echo "Hash: " . password_hash($argv[1], PASSWORD_DEFAULT) . "\n";
-} else {
-    echo "Usage: php gen_hash.php [your_password_here]\n";
-}
